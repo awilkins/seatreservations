e3d40ed5fb252f52b0aea65a8e050d542265f5be
Adrian Wilkins <adrian.wilkins@dwp.gsi.gov.uk>
2018-04-25 20:45:59 +0100

    First test

---
+++ src/test/java/uk/gov/dwp/codility/seatreservations/SolutionTest.java
@@ -0,0 +1,12 @@
+package uk.gov.dwp.codility.seatreservations;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void functionIsPresent() {
+        Solution solution = new Solution();
+        solution.solution(1, "String");
+    }
+}
