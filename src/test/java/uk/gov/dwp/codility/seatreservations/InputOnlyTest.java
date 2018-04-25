a9f8f46f6bda071242ea2fca827bf658e57a683a
Adrian Wilkins <adrian.wilkins@dwp.gsi.gov.uk>
2018-04-25 21:14:04 +0100

    Implement the Codility style input only test

---
+++ src/test/java/uk/gov/dwp/codility/seatreservations/InputOnlyTest.java
@@ -0,0 +1,50 @@
+package uk.gov.dwp.codility.seatreservations;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * This class is intended to replicate the Codility "input only" tests that
+ * the web app presents - the user can populate a resource file to pass inputs and expected
+ * outputs to the function
+ */
+public class InputOnlyTest {
+
+    private class Params {
+
+        public Params(String line) {
+            parse(line);
+        }
+
+        private void parse(String line) {
+            String[] parts = line.split("\\|");
+            N = Integer.parseInt(parts[0]);
+            S = parts[1];
+            returnValue = Integer.parseInt(parts[2]);
+        }
+
+        int N;
+        String S;
+        int returnValue;
+    }
+
+    @Test
+    public void testInputs() throws IOException {
+        InputStream inputStream = InputOnlyTest.class.getResourceAsStream("inputs.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        while(reader.ready()) {
+
+            String line = reader.readLine();
+            Params p = new Params(line);
+
+            Solution s = new Solution();
+            assertEquals(String.format("Failed for \"%s\"", line), p.returnValue, s.solution(p.N, p.S));
+        }
+    }
+}
