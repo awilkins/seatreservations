8b7b3bc17f4d9c00967db4bf1a955e0336ffcf00
Adrian Wilkins <adrian.wilkins@dwp.gsi.gov.uk>
2018-04-25 22:53:46 +0100

    Give base test valid input

--- src/test/java/uk/gov/dwp/codility/seatreservations/SolutionTest.java
+++ src/test/java/uk/gov/dwp/codility/seatreservations/SolutionTest.java
@@ -9,7 +9,7 @@ public class SolutionTest {
     @Test
     public void functionIsPresent() {
         Solution solution = new Solution();
-        solution.solution(1, "String");
+        solution.solution(1, "1A");
     }
 
     @Test
