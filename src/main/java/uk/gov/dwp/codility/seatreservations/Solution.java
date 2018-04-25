b01e4f8c9f83e78e8a845df963084e0fcdd199b5
Adrian Wilkins <adrian.wilkins@dwp.gsi.gov.uk>
2018-04-25 21:48:48 +0100

    makeRow() function

--- src/main/java/uk/gov/dwp/codility/seatreservations/Solution.java
+++ src/main/java/uk/gov/dwp/codility/seatreservations/Solution.java
@@ -1,6 +1,13 @@
 package uk.gov.dwp.codility.seatreservations;
 
+import com.google.common.annotations.VisibleForTesting;
+
 public class Solution {
+
+    @VisibleForTesting
+    static boolean[] makeRow() {
+        return new boolean[10];
+    }
 
     public int solution(int N, String S) {
         return 0;
