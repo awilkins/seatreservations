5040c689809ce72dae95cf3b41a2779863c0dc85
Adrian Wilkins <adrian.wilkins@dwp.gsi.gov.uk>
2018-04-25 21:48:28 +0100

    Test that new rows are 10 seats

--- src/test/java/uk/gov/dwp/codility/seatreservations/SolutionTest.java
+++ src/test/java/uk/gov/dwp/codility/seatreservations/SolutionTest.java
@@ -1,6 +1,8 @@
 package uk.gov.dwp.codility.seatreservations;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 public class SolutionTest {
 
@@ -8,5 +10,11 @@ public class SolutionTest {
     public void functionIsPresent() {
         Solution solution = new Solution();
         solution.solution(1, "String");
+    }
+
+    @Test
+    public void rowIsTenElements() {
+        boolean[] row = Solution.makeRow();
+        assertThat(row.length, is(10));
     }
 }
